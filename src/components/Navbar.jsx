acf4dc90d4b83d2b2997f25b15c31bb32dfc73c5
Muhammed Mert Araz <67739721+measses@users.noreply.github.com>
2024-04-03 23:53:46 +0300

    Update Navbar component with new navigation menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,34 @@ import "../assets/css/Navbar.css";
 function Navbar() {
   return (
     <div>
-      <h1 className="mukta-bold text-5xl mt-16 ml-14">Blog</h1>
+      <nav className="px-8 py-4">
+        <span className="text-4xl font-extrabold flex mt-14 mx-40 text-indigo-600 ">
+          blogyX
+        </span>
+        <div className="flex h-screen justify-center items-center">
+          <ul className="flex space-x-14 mb-[850px] ">
+            {[
+              "Kategoriler --->",
+              "Teknoloji",
+              "Yazılım",
+              "Sanat",
+              "Eğitim",
+              "Kişisel Gelişim",
+              "Spor",
+              "Oyun",
+            ].map((category) => (
+              <li className="border rounded px-4 py-2 hover:border-indigo-500 transition-colors bg-slate-300">
+                <a
+                  href="#"
+                  className="font-medium text-gray-600 hover:text-indigo-500"
+                >
+                  {category}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </nav>
     </div>
   );
 }
