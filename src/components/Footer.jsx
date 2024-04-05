88de5ee16f04350108fb39e79b85f75ab3c0125f
Mert Araz <mertoreyiz1907@gmail.com>
2024-04-05 18:30:59 +0300

    Add react-icons package and update Navbar and Footer components

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <div>
+      <footer className="bg-slate-900 text-white text-center py-4">
+        <p>&copy; 2024 blogyX -- Mert Araz</p>
+      </footer>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "../assets/css/Navbar.css";
+
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div>
       <nav className="px-8 py-4">
-        <span className="text-4xl font-extrabold flex mt-14 mx-40 text-indigo-600 ">
-          blogyX
-        </span>
-        <div className="flex h-screen justify-center items-center">
-          <ul className="flex space-x-14 mb-[850px] ">
+        <div className="flex justify-between items-center">
+          <span className="text-4xl font-extrabold text-indigo-600">
+            blogyX
+          </span>
+          <button className="lg:hidden" onClick={() => setIsOpen(!isOpen)}>
+            <svg
+              className="h-20 w-8"
+              fill="none"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path d="M4 6h16M4 12h16m-7 6h7" />
+            </svg>
+          </button>
+        </div>
+        <div
+          className={`${
+            isOpen ? "flex" : "hidden"
+          } lg:flex flex-col lg:flex-row lg:items-center h-screen lg:h-auto`}
+        >
+          <ul className="flex flex-col lg:flex-row sm:space-x-4 md:space-x-8 lg:space-x-14 sm:mb-0 mx-auto max-w-3/4 md:max-w-2/4 lg:max-w-1/2">
             {[
-              "Kategoriler --->",
+              "Kategoriler -->",
               "Teknoloji",
               "Yazılım",
               "Sanat",
@@ -18,8 +40,11 @@ function Navbar() {
               "Kişisel Gelişim",
               "Spor",
               "Oyun",
-            ].map((category) => (
-              <li className="border rounded px-4 py-2 hover:border-indigo-500 transition-colors bg-slate-300">
+            ].map((category, index) => (
+              <li
+                key={index}
+                className="border rounded px-4 py-2 hover:border-indigo-500 transition-colors bg-slate-100 my-2 sm:my-0"
+              >
                 <a
                   href="#"
                   className="font-medium text-gray-600 hover:text-indigo-500"
