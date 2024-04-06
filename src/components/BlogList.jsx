eb455b32304bc61388ec12737dec9896ec1b7d3e
Muhammed Mert Araz <67739721+measses@users.noreply.github.com>
2024-04-06 13:17:16 +0300

    Add useEffect and useState hooks to fetch and display blog list

--- src/components/BlogList.jsx
+++ src/components/BlogList.jsx
@@ -1,7 +1,80 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function BlogList() {
-  return <div>BlogList</div>;
+  const [blogs, setBlogs] = useState([]);
+
+  const fetchBlogList = async () => {
+    try {
+      const response = await fetch(import.meta.env.VITE_API_KEY);
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const data = await response.json();
+      setBlogs(data);
+    } catch (error) {
+      console.error("Fetch error: ", error);
+    }
+  };
+  useEffect(() => {
+    fetchBlogList();
+  }, []);
+
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-4 justify-items-center mt-8 mb-4">
+      {blogs.map((blog, index) => (
+        <div
+          key={index}
+          className="flex flex-col w-full sm:w-64 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 overflow-hidden"
+        >
+          <a href="#">
+            <img className="w-full h-48 object-cover" src={blog.photo} alt="" />
+          </a>
+          <div className="p-4 flex flex-col flex-grow">
+            <a href="#">
+              <h5 className="mb-2 text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                {blog.title}
+              </h5>
+            </a>
+            <p className="mb-3 flex-grow text-sm font-normal text-gray-700 dark:text-gray-400">
+              {blog.content}
+            </p>
+            <div className="flex flex-wrap gap-2 mb-4">
+              {blog.tags.map((tag, tagIndex) => (
+                <span
+                  key={tagIndex}
+                  className="text-xs font-medium bg-blue-100 text-blue-800 px-2 py-1 rounded-full"
+                >
+                  #{tag}
+                </span>
+              ))}
+            </div>
+
+            <a
+              href="#"
+              className="inline-flex items-center px-3 py-2 text-xs font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mt-auto"
+            >
+              Daha Fazla
+              <svg
+                className="ml-1 w-3 h-3"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 14 10"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M1 5h12m0 0L9 1m4 4L9 9"
+                />
+              </svg>
+            </a>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default BlogList;
