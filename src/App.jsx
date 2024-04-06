6fe377653fd081fde2602aec58f4b7b7ea5eaee9
Muhammed Mert Araz <67739721+measses@users.noreply.github.com>
2024-04-06 17:26:31 +0300

    Add routing and context to App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,25 @@ import BlogList from "./components/BlogList";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import "./App.css";
+import { BlogProvider } from "./components/BlogContext";
+import BlogDetail from "./components/BlogDetail";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   // console.log(import.meta.env.VITE_API_KEY);
   //loading items from the API
 
   return (
-    <div className="flex flex-col min-h-screen">
-      <Navbar />
-      <BlogList />
-      <Footer />
-    </div>
+    <BlogProvider>
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<BlogList />} />
+          <Route path="/blog/:id" element={<BlogDetail />} />
+        </Routes>
+        <Footer />
+      </div>
+    </BlogProvider>
   );
 }
 
