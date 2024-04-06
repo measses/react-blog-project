ccc3476715f3243f09d15e781d8546d43670f3a7
Muhammed Mert Araz <67739721+measses@users.noreply.github.com>
2024-04-06 15:14:03 +0300

    Add BlogList component and App.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
+import BlogList from "./components/BlogList";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
+import "./App.css";
 
 function App() {
-  console.log(import.meta.env.VITE_API_KEY);
+  // console.log(import.meta.env.VITE_API_KEY);
+  //loading items from the API
 
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
       <Navbar />
+      <BlogList />
+      <Footer />
     </div>
   );
 }
